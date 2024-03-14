0cd20d08625655cb5f609840ec6834282c18ac03
MAD_DOGGO <laleme0@gmail.com>
2024-03-14 11:01:16 +0900

    feat: UI 연동
    - Script Name Modified(UI_MainButton.cs)

--- DirectionAR/Assets/Scripts/UI/ButtonMapping/UI_MainButton.cs
+++ DirectionAR/Assets/Scripts/UI/ButtonMapping/UI_MainButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_MainButtonPopup : UI_Popup
+public class UI_MainButton : UI_Popup
 {
     enum Buttons
     {
