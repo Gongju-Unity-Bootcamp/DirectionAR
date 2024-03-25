7779262a0a4c4fc85324f26cbbfc7a50373fc42c
MAD_DOGGO <laleme0@gmail.com>
2024-03-25 18:02:55 +0900

    feat: 스크립트 개선 및 통합

--- DirectionAR/Assets/Scripts/Gps/NStaticMapManager.cs
+++ DirectionAR/Assets/Scripts/Gps/NStaticMapManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-
 public class NStaticMapManager : MonoBehaviour
 {
     [Header("맵 정보 입력")]
@@ -21,58 +20,88 @@ public class NStaticMapManager : MonoBehaviour
     public enum type { basic, traffic, satellite, satellite_base, terrain };
     public type mapType = type.basic;
 
-    public TextMeshProUGUI mapLog = new TextMeshProUGUI(); 
-
-    private int frame = 0;
-
     private float latitude = default;
     private float longitude = default;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         rect = gameObject.GetComponent<RawImage>().rectTransform.rect;
         mapWidth = (int)Math.Round(rect.width);
         mapHeight = (int)Math.Round(rect.height);
 
-        StartCoroutine(GetStaticMap());
+        Input.location.Start();
+        StartCoroutine(UpdateLocation());
     }
 
     // Update is called once per frame
     void Update()
     {
-        latitude = Input.location.lastData.latitude;
-        longitude = Input.location.lastData.longitude;
-
-        /*if (frame >= 100)
-        {
-            StartCoroutine(GetStaticMap());
-            frame = 0;
-        }
-        frame++;*/
     }
 
-    IEnumerator GetStaticMap()
+    // 위치 정보 업데이트 코루틴
+    IEnumerator UpdateLocation()
     {
-        url = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?" + "w=" + mapWidth.ToString() + "&h=" + mapHeight.ToString() + "&level=" + level.ToString();
+        while (true)
+        {
+            if (!Input.location.isEnabledByUser)
+            {
+                Managers.Android.ShowAndroidToastMessage("위치 서비스가 활성화되어 있지 않습니다.");
+                yield break;
+            }
 
-        var query = "";
-        query += "&center=" + UnityWebRequest.UnEscapeURL(string.Format("{0},{1}", latitude, longitude));
+            int maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);
+                maxWait--;
+            }
 
-        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url + query);
+            if (maxWait <= 0)
+            {
+                Managers.Android.ShowAndroidToastMessage("시간 초과: 위치 서비스를 시작할 수 없습니다.");
+                yield break;
+            }
+
+            if (Input.location.status == LocationServiceStatus.Running)
+            {
+                latitude = Input.location.lastData.latitude;
+                longitude = Input.location.lastData.longitude;
+
+                UpdateMap();
+            }
+            else
+            {
+                Managers.Android.ShowAndroidToastMessage("위치 서비스를 시작할 수 없습니다.");
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    // 맵 업데이트 함수
+    void UpdateMap()
+    {
+        url = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster?" + "w=" + mapWidth.ToString() + "&h=" + mapHeight.ToString() + $"&markers=type:d|size:mid|pos:{longitude} {latitude}" + "&center=" + $"{longitude} {latitude}" + "&level=" + level.ToString();
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
         www.SetRequestHeader("X-NCP-APIGW-API-KEY-ID", clientID);
         www.SetRequestHeader("X-NCP-APIGW-API-KEY", secretKey);
 
+        StartCoroutine(LoadMap(www));
+    }
+
+    // 맵 로딩 함수
+    IEnumerator LoadMap(UnityWebRequest www)
+    {
         yield return www.SendWebRequest();
 
         if (www.result == UnityWebRequest.Result.Success)
         {
             GetComponent<RawImage>().texture = DownloadHandlerTexture.GetContent(www);
-            mapLog.text = "택스쳐 불러오기 성공";
         }
         else
         {
-            mapLog.text = "error. 맵 불러오기 실패";
+            Managers.Android.ShowAndroidToastMessage($"error. 맵 불러오기 실패\n{www.result}");
         }
     }
 }
